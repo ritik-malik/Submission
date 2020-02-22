e8f36ef3aca1251e27664b11be197cc82605840c
maalik <ritik18406@iiitd.ac.in>
2020-02-23 01:34:52 +0530

    R commands for ballgown

--- ball_cmds.R
+++ ball_cmds.R
@@ -1,14 +1,20 @@
+# importing the ballgown library
 library(ballgown)
+
+# setting the working directory
 setwd('/home/michael/Desktop/G-Soc/Submission/ball_input')
 
+# importing data of tablemaker using ballgown
 bg_brain = ballgown(samples = 'output_brain/')
 bg_liver = ballgown(samples = 'output_liver/')
 bg_brain
 bg_liver
 
+# creating data.frames for FPKM from texpr funtion
 t_fpkm_brain = texpr(bg_brain, 'FPKM')
 t_fpkm_liver = texpr(bg_liver, 'FPKM')
 
+# writing the output to CSV files
 write.csv(t_fpkm_brain ,"/home/michael/Desktop/G-Soc/Submission/FPKM_brain.csv", row.names = FALSE)
 write.csv(t_fpkm_liver ,"/home/michael/Desktop/G-Soc/Submission/FPKM_liver.csv", row.names = FALSE)
 

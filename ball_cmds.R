d69b67afe2c97d2d2385c25fe7a6ab9c0e7dfcbd
maalik <ritik18406@iiitd.ac.in>
2020-02-23 01:48:49 +0530

    R commands for ballgown

--- ball_cmds.R
+++ ball_cmds.R
@@ -10,13 +10,13 @@ bg_liver = ballgown(samples = 'output_liver/')
 bg_brain
 bg_liver
 
-# creating data.frames for FPKM from texpr funtion
-t_fpkm_brain = texpr(bg_brain, 'FPKM')
-t_fpkm_liver = texpr(bg_liver, 'FPKM')
+# creating matrices for FPKM from texpr funtion
+tx_brain = texpr(bg_brain, 'all')
+tx_liver = texpr(bg_liver, 'all')
 
 # writing the output to CSV files
-write.csv(t_fpkm_brain ,"/home/michael/Desktop/G-Soc/Submission/FPKM_brain.csv", row.names = FALSE)
-write.csv(t_fpkm_liver ,"/home/michael/Desktop/G-Soc/Submission/FPKM_liver.csv", row.names = FALSE)
+write.csv(tx_brain ,"/home/michael/Desktop/G-Soc/Submission/tx_brain.csv", row.names = TRUE)
+write.csv(tx_liver ,"/home/michael/Desktop/G-Soc/Submission/tx_liver.csv", row.names = TRUE)
 
-# used "head -1001 FPKM_brain.csv > FPKM_brain_1000.csv" to get 1st 1000 lines output, use "cat FPKM_brain_1000.csv | wc -l" to verify
-# //ly for FPKM_liver.csv
+# used "head -1001 tx_brain.csv > tx_brain_1000.csv" to get 1st 1000 lines output, use "cat tx_brain_1000.csv | wc -l" to verify the same.
+# //ly for tx_liver.csv
